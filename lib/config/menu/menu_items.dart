3e746e80499665c3196a358a6fa2e258095e4474
Dario Arellano <darellano@greensolutions.mx>
2023-06-19 10:33:03 -0600

    Home screen implementado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -11,17 +11,17 @@ class MenuItem {
       required this.subTitle,
       required this.link,
       required this.icon});
-
-  static const appMenuItems = <MenuItem>[
-    MenuItem(
-        title: 'Botones',
-        subTitle: 'Varios botones en Flutter',
-        link: '/buttons',
-        icon: Icons.smart_button_outlined),
-    MenuItem(
-        title: 'Tarjetas',
-        subTitle: 'Un contenedor estilizado',
-        link: '/cards',
-        icon: Icons.credit_card),
-  ];
 }
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+      title: 'Botones',
+      subTitle: 'Varios botones en Flutter',
+      link: '/buttons',
+      icon: Icons.smart_button_outlined),
+  MenuItem(
+      title: 'Tarjetas',
+      subTitle: 'Un contenedor estilizado',
+      link: '/cards',
+      icon: Icons.credit_card),
+];
