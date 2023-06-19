3230c5a68d44bda0465000fbe0b81c000bb58e34
Dario Arellano <darellano@greensolutions.mx>
2023-06-19 11:11:42 -0600

    go router por name

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgetsapp/config/menu/menu_items.dart';
+import 'package:widgetsapp/presentation/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const String name = "home_screen";
   const HomeScreen({super.key});
 
   @override
@@ -49,7 +51,8 @@ class _CustomListTile extends StatelessWidget {
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
       onTap: () {
-        context.push(menuItem.link);
+        //context.push(menuItem.link);
+        context.pushNamed(CardsScreen.name);
       },
     );
   }
